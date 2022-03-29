4b1d4b750281d9d7f3165c63f8d82c7583d64153
Dozi <ytooom019@naver.com>
2022-03-30 00:24:22 +0900

    firebase 연결

---
+++ src/firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
--- src/routes/Customer.jsx
+++ src/routes/Customer.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Customer.module.css";
 import mainImg from "../images/Sample.jpg";
+import { app } from "../firebaseConfig";
+import { getDatabase, ref, set } from "firebase/database";
+
+function writeUserData(userId, name, email, phoneNum) {
+  const db = getDatabase();
+  set(ref(db, "users/" + userId), {
+    username: name,
+    email: email,
+    phoneNum: phoneNum,
+  });
+}
+// const db = getDatabase();
+// console.log(db);
+const newID = () => {
+  return Math.random().toString(36).substr(2, 16);
+};
 
 function Customer() {
+  const [values, setValues] = useState({ name: "", email: "", phoneNum: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    writeUserData(newID(), values.name, values.email, values.phoneNum);
+    alert("제출되었습니다!");
+    e.preventDefault();
+    setValues({ name: "", email: "", phoneNum: "" });
+  };
+
   const containerStyle = {
     backgroundImage: `url(${mainImg})`,
     width: "100vw",
@@ -11,15 +41,30 @@ function Customer() {
     <section className={styles.section} style={containerStyle}>
       <div className={styles.formContainer}>
         <h2 className={styles.title}>관심 고객 등록</h2>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <p type="이 름:">
-            <input className={styles.input} />
+            <input
+              value={values.name}
+              name="name"
+              className={styles.input}
+              onChange={handleChange}
+            />
           </p>
           <p type="E-mail:">
-            <input className={styles.input} />
+            <input
+              value={values.email}
+              name="email"
+              className={styles.input}
+              onChange={handleChange}
+            />
           </p>
           <p type="전화번호:">
-            <input className={styles.input} />
+            <input
+              value={values.phoneNum}
+              name="phoneNum"
+              className={styles.input}
+              onChange={handleChange}
+            />
           </p>
 
           <button className={styles.button} type="submit" href="/">
