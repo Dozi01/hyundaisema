4d38433f62a41b2c5d8fdbdb41fb328c9491773e
Dozi <ytooom019@naver.com>
2022-04-14 00:49:10 +0900

    navbar 반응형

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,17 +5,28 @@ import { Group_obj, Group_key_arr } from "../atom/NavList";
 import mainLogo from "../img/logo-removebg.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPhoneFlip,
+  faBars,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Nav() {
   // scroll, hover 상태에 따라 nav css 변경
   const [Hover, setHover] = useState(false);
-
+  const [menuOpen, setMenuOpen] = useState(false);
   const handleMouseHover = () => {
     if (Hover) {
       setHover(false);
     } else {
       setHover(true);
+    }
+  };
+  const menuButton = () => {
+    if (menuOpen) {
+      setMenuOpen(false);
+    } else {
+      setMenuOpen(true);
     }
   };
 
@@ -28,19 +39,28 @@ function Nav() {
   };
 
   return (
-    <div>
+    <div className={styles.container}>
       <div
-        className={styles.container}
+        className={styles.navContainer}
         style={Hover ? white : transparent}
         onMouseEnter={handleMouseHover}
         onMouseLeave={handleMouseHover}
       >
+        <div className={styles.menuButtonContainer}>
+          <button className={styles.menuButton} onClick={menuButton}>
+            {menuOpen ? (
+              <FontAwesomeIcon icon={faXmark} />
+            ) : (
+              <FontAwesomeIcon icon={faBars} />
+            )}
+          </button>
+        </div>
         <div>
           <Link to={`/`}>
-            <img className={styles.Logo} src={mainLogo} alt="hyundaisemaLogo" />
+            <img className={styles.logo} src={mainLogo} alt="hyundaisemaLogo" />
           </Link>
         </div>
-        <div className={styles.NavOptionList}>
+        <div className={styles.navOptionList}>
           {Group_key_arr.map((key) => {
             return (
               <div className={styles.Link} key={key}>
@@ -51,11 +71,27 @@ function Nav() {
             );
           })}
         </div>
+
         <button className={styles.phoneCall}>
           <FontAwesomeIcon icon={faPhoneFlip} />
           <a href="tel:16669338">1666-9338</a>
         </button>
       </div>
+      {menuOpen ? (
+        <div className={styles.navOptionListOpen}>
+          {Group_key_arr.map((key) => {
+            return (
+              <div className={styles.Link} key={key}>
+                <div className={styles.Link_sep}>
+                  <Link to={`/${Group_obj[key]}`}>{key}</Link>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
